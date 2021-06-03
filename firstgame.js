32a46181b14d0577f2e246d9b7632616bd623727
Shareena Begum <shareena.fsp@gmail.com>
2021-06-03 20:50:07 +0200

    blackjack game has completed

--- firstgame.js
+++ firstgame.js
@@ -1,16 +1,37 @@
-let firstCard = 10
-let secondCard = 6
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasBlackJack = false
 let isAlive = true
 let message = " "
-
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
+let player = {
+    name :"Shanna",
+    chips : 145
+}
+
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
+
+function getRandomCard() {
+    let randomNumer = Math.floor (Math.random()*13)+1
+    if (randomNumer > 10){
+        return 10
+    } else if (randomNumer === 1){
+        return 11
+    } else {
+        return randomNumer
+    }
+}
+
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderGame()
 }
 function renderGame() {
@@ -34,11 +55,13 @@ function renderGame() {
 }
 
 function newCard() {
-    let card = 4
-    sum+= card
-    cards.push(card)
-    console.log(cards)
-    renderGame()
+    
+    if (isAlive === true && hasBlackJack === false){
+        let card = getRandomCard ()
+        sum+= card
+        cards.push(card)
+        renderGame()    
+    }
 }
 
 
